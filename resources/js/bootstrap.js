3026675efedc5c780c977c0d5c510b94fdbe6a57
hokeryu318 <hokelucpy@outlook.com>
2019-10-07 12:33:32 +0800

    .gitignore is now working

--- resources/js/bootstrap.js
+++ resources/js/bootstrap.js
@@ -50,8 +50,7 @@ window.Pusher = require('pusher-js');
 
 window.Echo = new Echo({
     broadcaster: 'pusher',
-    key: process.env.MIX_PUSHER_APP_KEY,
-    wsHost: window.location.hostname,
-    wsPort: 6001,
-    disableStats: true,
+    key: 'd653f36743160fcff12e',
+    cluster: 'ap3',
+    forceTLS: true
 });
