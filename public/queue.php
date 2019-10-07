c52a83fed7f43e1f9ec590d9409a478b1f6a6599
hokeryu318 <hokelucpy@outlook.com>
2019-10-07 16:23:52 +0800

    queue

--- public/queue.php
+++ public/queue.php
@@ -1,3 +1,4 @@
 <?php
-    // echo exec('nohup /usr/local/php/7.1/bin/php ../artisan queue:work --daemon &');
-    echo exec('ps -ef | grep nohup');
+    echo exec('nohup /usr/local/php/7.1/bin/php ../artisan queue:work --daemon > my.log 2>&1 &
+    echo $! > save_pid.txt &');
+    // echo exec('ps -ef | grep nohup');
