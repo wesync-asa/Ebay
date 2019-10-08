02267084e5aebb22d8df82fae2ed122dff1566dc
hokeryu318 <hokelucpy@outlook.com>
2019-10-08 09:48:50 +0800

    nohup killed why

--- public/queue.php
+++ public/queue.php
@@ -1,4 +1,4 @@
 <?php
     echo exec('nohup /usr/local/php/7.1/bin/php ../artisan queue:work --daemon > my.log 2>&1 &
-    echo $! > save_pid.txt &');
+    echo $! > save_pid.txt & disown');
     // echo exec('ps -ef | grep nohup');
