7b061fbb87f3812755e199bcf32fc25972ba61c0
hokeryu318 <hokelucpy@outlook.com>
2019-10-08 09:39:00 +0800

    nohup killed why

--- public/queue.php
+++ public/queue.php
@@ -1,4 +1,4 @@
 <?php
-    echo exec('systemd-run --scope --raindrop.jp-blueseason nohup /usr/local/php/7.1/bin/php ../artisan queue:work --daemon > my.log 2>&1 &
+    echo exec('nohup /usr/local/php/7.1/bin/php ../artisan queue:work --daemon > my.log 2>&1 &
     echo $! > save_pid.txt &');
     // echo exec('ps -ef | grep nohup');
